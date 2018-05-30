0f2e39291312066ae390176602eb1a8bfb685ea6
Unknown <azofeifacris4@gmail.com>
2018-05-29 22:50:47 -0600

    12

---
+++ cmake-build-debug/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/Chunk.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/Chunk.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/DataBase/Metadata.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/DataBase/Metadata.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/DataBase/Query.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/DataBase/Query.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/DataBase/Userdata.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/DataBase/Userdata.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/Oddysey_server.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/Oddysey_server.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/base64.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/base64.cpp.o"
+  "/home/kimberlyc/CLionProjects/OddyseyServer1-/pugixml.cpp" "/home/kimberlyc/CLionProjects/OddyseyServer1-/cmake-build-debug/CMakeFiles/Main.dir/pugixml.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Main.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main.dir/base64.cpp.o"
+  "CMakeFiles/Main.dir/Chunk.cpp.o"
+  "CMakeFiles/Main.dir/Oddysey_server.cpp.o"
+  "CMakeFiles/Main.dir/pugixml.cpp.o"
+  "CMakeFiles/Main.dir/DataBase/Metadata.cpp.o"
+  "CMakeFiles/Main.dir/DataBase/Query.cpp.o"
+  "CMakeFiles/Main.dir/DataBase/Userdata.cpp.o"
+  "Main.pdb"
+  "Main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
